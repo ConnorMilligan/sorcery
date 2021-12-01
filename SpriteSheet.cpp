9daee80d2ed2e8c593ad6096ba3f67f4ed15b7f3
Connor Milligan <connoramilligan@gmail.com>
2021-12-01 17:32:36 -0500

    Some new sprites

--- SpriteSheet.cpp
+++ SpriteSheet.cpp
@@ -4,9 +4,11 @@
 
 //Monster head sprites
 #include "sprites/mHead_hawk.h"
+#include "sprites/mHead_cat.h"
 
 //Monster body sprites
 #include "sprites/mBody_weightLifter.h"
+#include "sprites/mBody_octopus.h"
 
 SpriteSheet::SpriteSheet() {
 	this->gameLogo = Sprite({
@@ -24,12 +26,26 @@ SpriteSheet::SpriteSheet() {
 		mHead_hawkSprite
 	});
 
+	this->monsterHeads.push_back({
+		mHead_catName,
+		mHead_catWidth,
+		mHead_catHeight,
+		mHead_catSprite
+	});
+
 	//Adding in the monsterBodies
 	this->monsterBodies.push_back({
 		mBody_weightLifterName,
 		mBody_weightLifterWidth,
 		mBody_weightLifterHeight,
 		mBody_weightLifterSprite
+	});
+
+	this->monsterBodies.push_back({
+		mBody_octopusName,
+		mBody_octopusWidth,
+		mBody_octopusHeight,
+		mBody_octopusSprite
 	});
 }
 
